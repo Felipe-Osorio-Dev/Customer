f7fa520dcf99140c0338444ab62278c47ed9d5fa
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-04-04 17:43:02 -0300

    feat: Criacao do Container Principal da aplicacao.

--- CustomerAPP/Views/Main/MainForm.cs
+++ CustomerAPP/Views/Main/MainForm.cs
@@ -1,10 +1,19 @@
+using CustomerAPP.Views.Main;
+
 namespace CustomerAPP
 {
-    public partial class MainForm : Form
+    public partial class MainForm : Form, IMainView
     {
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        public event EventHandler ClickCustomerRegister;
+
+        private void menuItemCustomer_Click(object sender, EventArgs e)
+        {
+            ClickCustomerRegister?.Invoke(this, EventArgs.Empty);
         }
     }
 }
