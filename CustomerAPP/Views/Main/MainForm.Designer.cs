32daf011776fed048a162560353ad18968e9e942
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-04-05 00:07:16 -0300

    feat: Implementacao da Tela que sera a Janela principal do App.

--- CustomerAPP/Views/Main/MainForm.Designer.cs
+++ CustomerAPP/Views/Main/MainForm.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            menuStrip1 = new MenuStrip();
+            cadastroToolStripMenuItem = new ToolStripMenuItem();
+            menuItemCustomer = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cadastroToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastroToolStripMenuItem
+            // 
+            cadastroToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { menuItemCustomer });
+            cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
+            cadastroToolStripMenuItem.Size = new Size(66, 20);
+            cadastroToolStripMenuItem.Text = "Cadastro";
+            // 
+            // menuItemCustomer
+            // 
+            menuItemCustomer.Name = "menuItemCustomer";
+            menuItemCustomer.Size = new Size(180, 22);
+            menuItemCustomer.Text = "Cliente";
+            menuItemCustomer.Click += menuItemCustomer_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "MainForm";
+            Text = "Customer";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem cadastroToolStripMenuItem;
+        private ToolStripMenuItem menuItemCustomer;
     }
 }
