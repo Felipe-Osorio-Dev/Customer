f9b10c987b6a9dc60dd367b55b9124548b6e1e25
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-04-05 00:06:25 -0300

    feat: Implementado classe responsavel pela navegacao do App.

---
+++ CustomerAPP/Service/Navigation/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerAPP.Service.Navigation
+{
+    internal interface INavigationService
+    {
+        void NavigateTo<TForm, TPresenter>() where TForm : Form where TPresenter : class, new();
+
+        void SetMdiParent(Form mdiParent);
+    }
+}
---
+++ CustomerAPP/Service/Navigation/NavigationService.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomerAPP.Service.Navigation
+{
+    internal class NavigationService : INavigationService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private Form? _mdiParent;
+
+        public NavigationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void NavigateTo<TForm, TPresenter>() where TForm : Form where TPresenter : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMdiParent(Form mdiParent)
+        {
+            _mdiParent = mdiParent;
+        }
+    }
+}
