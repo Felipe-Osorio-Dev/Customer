cc8c0ce1a388a12f2d0ceebf2775834ed71febca
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-04-05 00:05:58 -0300

    feat: Implementado MainPresenter para lidar com os eventos do MainContainer.

---
+++ CustomerAPP/Presenters/MainPresenter.cs
@@ -0,0 +1,24 @@
+﻿using CustomerAPP.Service.Navigation;
+using CustomerAPP.Views.Main;
+
+namespace CustomerAPP.Presenters
+{
+    internal class MainPresenter
+    {
+        private readonly INavigationService _navigationService;
+        private readonly IMainView _mainView;
+
+        public MainPresenter(IMainView mainView, INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            _mainView = mainView;
+
+            _mainView.ClickCustomerRegister += OnClickCustomerRegister;
+        }
+
+        private void OnClickCustomerRegister(object sender, EventArgs e)
+        {
+            MessageBox.Show("TESTANDO");
+        }
+    }
+}
