45456cf3be428a823d390179f570d1f1b3c777e2
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-04-13 00:41:19 -0300

    feat: Enum criado para identificar qual o Tipo de documento do customer e popular o combobox.

---
+++ CustomerAPP/Util/CustomerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAPP.Util
+{
+    public enum CustomerType
+    {
+        CPF = 1,
+        CNPJ = 2
+    }
+}
