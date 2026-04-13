e826cb645ab358b8536e0b3964a4b9cd323fa43f
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-04-13 00:42:01 -0300

    feat: Criado requests para receber os dados dos fields da tela de registro.

---
+++ CustomerAPP/Models/Request/CustomerAddressRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomerAPP.Models.Request
+{
+    public class CustomerAddressRequest
+    {
+        public string Street { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public string Neighborhood { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+    }
+}
---
+++ CustomerAPP/Models/Request/CustomerRegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerAPP.Models.Request
+{
+    public class CustomerRegisterRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty;
+        public int CustomerType { get; set; } = 1;
+
+        public CustomerAddressRequest Address { get; set; } = new();
+    }
+}
