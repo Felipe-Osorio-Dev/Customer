2a586006dc855e09a053eae4e43b5e3a84b50f9a
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-04-13 00:40:29 -0300

    feat: Classe criada para enviar dados customizados do tipo EventArgs.

---
+++ CustomerAPP/Views/Register/CustomerEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using CustomerAPP.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAPP.Views.Register
+{
+    public class CustomerEventArgs : EventArgs
+    {
+        public CustomerRegisterRequest _request { get; }
+
+        public CustomerEventArgs(CustomerRegisterRequest request)
+        {
+            _request = request;
+        }
+    }
+}
