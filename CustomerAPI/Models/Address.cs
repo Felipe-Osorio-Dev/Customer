e37e424a0f29b1584077d5d8ec754ec61cd38aa4
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-04-17 04:50:39 -0300

    feat: Criacao dos Models para representar as tabelas no DB.

---
+++ CustomerAPI/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomerAPI.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+
+    }
+}
---
+++ CustomerAPI/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using CustomerAPI.Util;
+
+namespace CustomerAPI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public CustomerType Type { get; set; }
+        public string Document { get; set; }
+        public bool IsActive { get; set; }
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public DateOnly? UpdateAt { get; set; }
+
+        public Address? Address { get; set; }
+    }
+}
